193aa41c190e3227870d3c7577995c04f36f03fd
Robert-Stefan <robertboss05@hotmail.it>
2021-05-09 17:29:45 +0200

    Add filter by genre finish

--- src/components/Musiclist.vue
+++ src/components/Musiclist.vue
@@ -1,25 +1,49 @@
 <template>
-    <div v-if="!loading" class="music">
-        <Music v-for="(music, index) in musicList" :key="index" :info="music" />
-    </div>
-    <div v-else class="loading">LOADING ...</div>
+    <section>
+        <Select @changed="filteredGenre" />
+        <div v-if="!loading" class="music">
+            <Music
+                v-for="(music, index) in newList"
+                :key="index"
+                :info="music"
+            />
+        </div>
+        <div v-else class="loading">LOADING ...</div>
+    </section>
 </template>
 
 <script>
 import axios from 'axios';
 import Music from '@/components/Music.vue';
+import Select from '@/components/Select.vue';
 
 export default {
     name: 'Musiclist',
     components: {
         Music,
+        Select,
     },
     data() {
         return {
             apiURL: 'https://flynn.boolean.careers/exercises/api/array/music',
             musicList: [],
             loading: true,
+            genreSelected: 'all',
         };
+    },
+    computed: {
+        newList() {
+            if (this.genreSelected === 'all') {
+                return this.musicList;
+            }
+
+            return this.musicList.filter(album => {
+                return (
+                    album.genre.toLowerCase() ===
+                    this.genreSelected.toLowerCase()
+                );
+            });
+        },
     },
     created() {
         this.getMusic();
@@ -32,13 +56,17 @@ export default {
             axios
                 .get(this.apiURL)
                 .then(res => {
-                    console.log(res.data.response);
+                    //console.log(res.data.response);
                     this.musicList = res.data.response;
                     this.loading = false;
                 })
                 .catch(err => {
                     console.log('Error', err);
                 });
+        },
+        filteredGenre(select) {
+            //console.log(this.genreSelected);
+            this.genreSelected = select;
         },
     },
 };
@@ -49,7 +77,6 @@ export default {
     display: flex;
     justify-content: center;
     flex-wrap: wrap;
-    padding-top: 110px;
 }
 
 .loading {
---
+++ src/components/Select.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="select">
+        <label for="genre-select">Filter by genre:</label>
+        <select
+            name="genre"
+            id="genre-select"
+            v-model="selected"
+            @change="$emit('changed', selected)"
+        >
+            <option value="all">All</option>
+            <option value="rock">Rock</option>
+            <option value="pop">Pop</option>
+            <option value="jazz">Jazz</option>
+            <option value="metal">Metal</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Select',
+    data() {
+        return {
+            selected: 'all',
+        };
+    },
+};
+</script>
+
+<style scoped lang="scss">
+.select {
+    background-color: #1d2d3c;
+    text-align: center;
+    padding: 50px;
+    color: #fff;
+    label {
+        margin-right: 20px;
+    }
+    select {
+        background-color: #2e3a46;
+        color: white;
+        outline: none;
+        padding: 5px 10px;
+        border: 0;
+        border-radius: 5px; // Adoro il border radius :D
+    }
+}
+</style>
